3a09a52b1356c093a4997e0b7cb726d374d79b96
HARSHITJOSHI1102 <harshitjoshi1102@gmail.com>
2025-04-06 02:17:47 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,4 +4,4 @@ export const connectDB = async() =>{
     await mongoose.connect("").then(()=>console.log("DB Connected"))
 } 
 
-//mongodb+srv://Harshitdb:<db_password>@cluster0.kisp2xa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
