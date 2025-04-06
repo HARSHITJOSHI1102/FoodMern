042a6a044099d729d12845eb3e9dfce6eef41d04
Harshit <harshitjoshi1102@gmail.com>
2025-04-07 02:08:55 +0530

    Till Date Updates

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -2,8 +2,8 @@ import React, { useState } from 'react'
 import "./Add.css"
 import { assets } from '../../assets/assets'
 import axios from "axios"
-const Add = () => {
-  const url = "http://localhost:4000";
+import { toast } from 'react-toastify'
+const Add = ({url}) => {
   const [image, setImage] = useState(false)
   const [data, setData] = useState({
     name: "",
@@ -17,16 +17,16 @@ const Add = () => {
     setData(data => ({ ...data, [name]: value }))
   }
 
-  const onSubmitHandler = async (event)=>{
+  const onSubmitHandler = async (event) => {
     event.preventDefault();
     const formData = new FormData();
-    formData.append("name",data.name)
-    formData.append("description",data.description)
-    formData.append("price",Number(data.price))
-    formData.append("category",data.category)
-    formData.append("image",image)
-    const response = await axios.post(`${url}/api/food/add`,formData)
-    if(response.data.success){
+    formData.append("name", data.name)
+    formData.append("description", data.description)
+    formData.append("price", Number(data.price))
+    formData.append("category", data.category)
+    formData.append("image", image)
+    const response = await axios.post(`${url}/api/food/add`, formData)
+    if (response.data.success) {
       setData(
         {
           name: "",
@@ -36,9 +36,10 @@ const Add = () => {
         }
       )
       setImage(false)
+      toast.success(response.data.message)
     }
-    else{
-
+    else {
+      toast.error(response.data.message)
     }
   }
 
--- frontend/src/components/LoginPopUp/LoginPopUp.jsx
+++ frontend/src/components/LoginPopUp/LoginPopUp.jsx
@@ -1,28 +1,62 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import "./LoginPopUp.css"
 import { assets } from '../../assets/assets'
+import { StoreContext } from '../../context/StoreContext'
+import axios from "axios"
 const LoginPopUp = ({ setShowLogin }) => {
-
+    const { url, setToken } = useContext(StoreContext)
     const [currState, setCurrState] = useState("Login")
+    const [data, setData] = useState({
+        name: "",
+        email: "",
+        password: "",
+    })
+
+    const onChangeHandler = (event) => {
+        const name = event.target.name
+        const value = event.target.value
+        setData(data => ({ ...data, [name]: value }))
+    }
+    const onLogin = async (event) => {
+        event.preventDefault();
+        let newUrl = url;
+        if (currState === "Login") {
+            newUrl += "/api/user/login"
+        }
+        else {
+            newUrl += "/api/user/register"
+        }
+        const response = await axios.post(newUrl,data);
+        if (response.data.success) {
+            setToken(response.data.token);
+            localStorage.setItem("token", response.data.token);
+            setShowLogin(false)
+        }
+        else{
+            alert(response.data.message)
+        }
+    }
     return (
         <div className='login-popup'>
-            <form className='login-popup-container'>
+            <form onSubmit={onLogin} className='login-popup-container'>
                 <div className="login-popup-title">
                     <h2>{currState}</h2>
                     <img src={assets.cross_icon} onClick={() => setShowLogin(false)} alt="" />
                 </div>
                 <div className='login-popup-inputs'>
-                    {currState === "Login" ? <></> : <input type="text" placeholder='Your name' required />}
+                    {currState === "Login" ? <></> : <input name="name" type="text" onChange={onChangeHandler} value={data.name} placeholder='Your name' required />}
 
-                    <input type="email" placeholder='Your email' required />
-                    <input type="password" placeholder='password' required />
+                    <input type="email" name='email' onChange={onChangeHandler}
+                        value={data.email} placeholder='Your email' required />
+                    <input name='password' onChange={onChangeHandler}
+                        value={data.password} type="password" placeholder='password' required />
                 </div>
-                <button>{currState === "Sign Up" ? "Create account" : "Login"}</button>
+                <button type='submit'>{currState === "Sign Up" ? "Create account" : "Login"}</button>
                 <div className="login-popup-condition">
                     <input type="checkbox" required />
                     <p>By Continuing , I Agree To The Terms of Use &Privacy Policy</p>
                 </div>
-                {currState === "Login" ? <p>Create A New Account? <span onClick={()=>setCurrState("Sign Up")}>Click Here</span></p> : <p>Already Have An Account? <span onClick={()=>setCurrState("Login")}>Login Here</span></p>}
+                {currState === "Login" ? <p>Create A New Account? <span onClick={() => setCurrState("Sign Up")}>Click Here</span></p> : <p>Already Have An Account? <span onClick={() => setCurrState("Login")}>Login Here</span></p>}
 
 
             </form>
