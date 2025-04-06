51a59ed17b5266e8b0a33b1a1ca59f2898ff933b
Harshit <harshitjoshi1102@gmail.com>
2025-04-07 02:11:02 +0530

    Latest

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async() =>{
-    await mongoose.connect("mongodb+srv://Harshitdb:Hj8joshi1234@cluster0.kisp2xa.mongodb.net/").then(()=>console.log("DB Connected"))
+    await mongoose.connect().then(()=>console.log("DB Connected"))
 } 
 
 
