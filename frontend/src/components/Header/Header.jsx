dd2ed2a0a196bc710aa4ed7933de1442e617756a
Harshit <harshitjoshi1102@gmail.com>
2025-04-10 02:26:09 +0530

    updates

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import "./Header.css"
+import React from 'react';
+import "./Header.css";
+import headerImg from '../../assets/header_img.png'; // adjust path if needed
+
 const Header = () => {
   return (
     <div className='header'>
+      <img src={headerImg} alt="Delicious Food" className="header-img" />
       <div className="header-contents">
         <h2>Order your favourite food here</h2>
-        <p>Choose From a diverse menu featuring a delectable array of dishes crafted with the finest ingredient and culinary expertise .Our Mission is to satisfy your craving and elevate your dining experience , one delicious meal at a time</p>
+        <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
         <button>View Menu</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
